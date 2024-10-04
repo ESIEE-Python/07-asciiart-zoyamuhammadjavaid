4325178b8f280ab14cd3eda069ec80095440b63d
zoyamuhammadjavaid <zoya.muhammadjavaid@edu.esiee.fr>
2024-10-04 21:19:38 +0000

    voici le programme sur le chapt des tuples

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#### Imports et définition des variables globales
+"""Voici le programme relatif aux tuples qui prend en compte deux fonctions secondaires"""
 
 
 #### Fonctions secondaires
-
+"""Cette fonction se base sur la méthode de l'iteration"""
 
 def artcode_i(s):
     """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
@@ -13,13 +13,25 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    if not s:
+        return []
+    c = [s[0]]
+    o = [1]
 
-    return [ ]
+    k = 1
 
+    while k < len(s):
+        if s[k] == s[k-1]:
+            o[-1] += 1
+        else :
+            c.append(s[k])
+            o.append(1)
+        k += 1
+    return list(zip(c,o))
 
 def artcode_r(s):
+    """Cette fonction se base sur la méthode de la recursivite"""
+
     """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
 
     Args:
@@ -28,19 +40,22 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    if not s:
+        return []
+    compt = 1
+
+    while compt < len(s) and s[compt] == s[0]:
+        compt += 1
+    tuples = (s[0], compt)
+    return [tuples] + artcode_r(s[compt:])
 
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
 
-    return []
-    
-
 #### Fonction principale
 
-
+"""Ici la fonction principale faita appel aux deux fonctions secondaires"""
 def main():
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
